ba777980d04702fb0c3d7e1f44a0dd03057ab705
Pa2Z <ryan.afnan03@gmail.com>
2025-12-28 21:27:58 +0600

    1st push

---
+++ signuppage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>NeedSurveyResponses - Sign Up</title>
+</head>
+
+<body>
+
+<header>
+  <div>
+    <div></div>
+    <div>NeedSurveyResponses</div>
+  </div>
+</header>
+
+<main>
+</main>
+
+</body>
+</html>
