784281997d6e9ded7b33e52cb0b5b0ed9567ae90
Pa2Z <ryan.afnan03@gmail.com>
2025-12-28 21:31:57 +0600

    2nd push Inputboxes

--- signuppage.php
+++ signuppage.php
@@ -15,7 +15,31 @@
 </header>
 
 <main>
+  <div>
+
+    <h1>Create an account</h1>
+
+    <form method="post" action="signup.php">
+
+      <label>Full Name</label>
+      <input type="text" name="fullname">
+
+      <label>Email Address</label>
+      <input type="email" name="email">
+
+      <label>Password</label>
+      <input type="password" name="password">
+
+      <label>Confirm Password</label>
+      <input type="password" name="confirm_password">
+
+      <button type="submit">Sign Up</button>
+
+    </form>
+
+  </div>
 </main>
+
 
 </body>
 </html>
